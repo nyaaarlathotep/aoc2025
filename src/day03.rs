da537a796e1d919573bb631a55c61357a81dc97c
nyaaar <nyaaarlathotep@163.com>
2025-12-03 18:44:56 +0800

    day03part2

--- src/day03.rs
+++ src/day03.rs
@@ -27,7 +27,37 @@ fn biggest(ori: String, new_char: u8) -> String {
 }
 
 pub fn part02(input: &str) -> Result<String, Error> {
-    Ok("solved part 2".to_string())
+    let res: i64 = input
+        .lines()
+        .map(|line| {
+            let mut chars = line.chars();
+            let mut biggest_num: String = chars.by_ref().take(12).collect();
+            for c in chars {
+                biggest_num = biggest_part2(biggest_num, c as u8);
+            }
+            biggest_num.parse::<i64>().unwrap()
+        })
+        .sum();
+
+    Ok(res.to_string())
+}
+
+fn biggest_part2(mut ori: String, new_char: u8) -> String {
+    let ori_bytes = ori.as_bytes();
+
+    for i in 1..ori.len() {
+        if ori_bytes[i] - b'0' > (ori_bytes[i - 1] - b'0') {
+            ori.remove(i - 1);
+            ori.push(new_char as char);
+            return ori;
+        }
+    }
+    if new_char - b'0' > ori_bytes[ori.len() - 1] - b'0' {
+        ori.remove(ori.len() - 1);
+        ori.push(new_char as char);
+        return ori;
+    }
+    ori
 }
 
 #[cfg(test)]
@@ -47,6 +77,6 @@ mod tests {
     fn test_part2() {
         // 读取同目录下的 test 文件
 
-        assert_eq!(part02(&INPUT).unwrap(), "6");
+        assert_eq!(part02(&INPUT).unwrap(), "3121910778619");
     }
 }
