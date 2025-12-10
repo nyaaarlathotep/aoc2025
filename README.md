459e69d2410aa7df80671fbcd005dc417d1a56c1
nyaaar <nyaaarlathotep@163.com>
2025-12-10 12:27:44 +0800

    day09part2

--- README.md
+++ README.md
@@ -11,3 +11,13 @@ I attempted to optimize the solution to meet timing constraints, but I realized 
 ## day08
 
 Really? How about UnionFind, that's how ai works.
+
+## day09
+
+I spent a long time trying to figure out how to determine if a rectangle is inside a polygon. I tried many methods, but they were either incorrect or simply inefficient.
+
+Thanks to Reddit, I found the best approach so far:
+
+Given the problem's constraints that edges are vertical or horizontal and the rectangle's vertices align with the edges—we can use a specific logic: if a rectangle is not totally inside the polygon, it must contain at least one complete polygon edge.
+
+Therefore, we can simply solve for the largest rectangle area that contains no edges. This approach is much easier to implement.
--- src/day09.rs
+++ src/day09.rs
@@ -1,5 +1,3 @@
-use std::f32::consts::E;
-
 #[derive(Clone)]
 struct Point {
     x: i64,
@@ -15,6 +13,12 @@ impl Point {
         let x = parts[0].trim().parse::<i64>().ok()?;
         let y = parts[1].trim().parse::<i64>().ok()?;
         Some(Point { x, y })
+    }
+
+    fn rect_area_with(&self, other: &Point) -> u64 {
+        let width = self.x.abs_diff(other.x) + 1;
+        let height = self.y.abs_diff(other.y) + 1;
+        width * height
     }
 }
 
@@ -52,112 +56,32 @@ pub fn part02(input: &str) -> Result<String, &str> {
             p2: points[(i + 1) % points.len()].clone(),
         });
     }
+    let mut possible_rects: Vec<_> = points
+        .iter()
+        .enumerate()
+        .flat_map(|(i, p1)| {
+            points[i + 1..]
+                .iter()
+                .map(move |p2| (p1, p2, p1.rect_area_with(p2)))
+        })
+        .collect();
 
-    let mut max_area = 0;
-    for i in 0..points.len() {
-        for j in i + 1..points.len() {
-            let p1 = &points[i];
-            let p2 = &points[j];
-            if (p1.x == p2.x) || (p1.y == p2.y) {
-                // line
-                if !no_intersect(
-                    &Line {
-                        p1: p1.clone(),
-                        p2: p2.clone(),
-                    },
-                    &lines,
-                ) {
-                    continue;
-                }
-                let area = (points[i].x - points[j].x + 1) * (points[i].y - points[j].y + 1);
-                max_area = if area > max_area { area } else { max_area };
-            } else {
-                // square
-                let line1 = Line {
-                    p1: p1.clone(),
-                    p2: Point { x: p2.x, y: p1.y },
-                };
-                if !no_intersect(&line1, &lines) {
-                    continue;
-                }
+    possible_rects.sort_by(|a, b| b.2.cmp(&a.2));
 
-                let line2 = Line {
-                    p1: Point { x: p2.x, y: p1.y },
-                    p2: p2.clone(),
-                };
-                if !no_intersect(&line2, &lines) {
-                    continue;
-                }
-                let line3 = Line {
-                    p1: p2.clone(),
-                    p2: Point { x: p1.x, y: p2.y },
-                };
-                if !no_intersect(&line3, &lines) {
-                    continue;
-                }
-                let line4 = Line {
-                    p1: Point { x: p1.x, y: p2.y },
-                    p2: p1.clone(),
-                };
-                if !no_intersect(&line4, &lines) {
-                    continue;
-                }
-                let area = (points[i].x - points[j].x + 1) * (points[i].y - points[j].y + 1);
-                max_area = if area > max_area { area } else { max_area };
-            }
-        }
-    }
-
-    Ok(max_area.to_string())
-}
-
-fn no_intersect(line: &Line, lines: &Vec<Line>) -> bool {
-    for l in lines {
-        if intersect(line, l) {
-            return false;
-        }
-    }
-    true
-}
-
-fn intersect(line1: &Line, line2: &Line) -> bool {
-    if line1.p1.x == line1.p2.x && line2.p1.x == line2.p2.x {
-        return false; // both vertical
-    }
-    if line1.p1.y == line1.p2.y && line2.p1.y == line2.p2.y {
-        return false; // both horizontal
-    }
-    if line1.p1.x == line1.p2.x {
-        // line1 vertical line2 horizontal
-        let line1_x = line1.p1.x;
-        let line1_y_min = line1.p1.y.min(line1.p2.y);
-        let line1_y_max = line1.p1.y.max(line1.p2.y);
-        let line2_x_min = line2.p1.x.min(line2.p2.x);
-        let line2_x_max = line2.p1.x.max(line2.p2.x);
-        if line1_x < line2_x_min || line1_x > line2_x_max {
-            return false;
-        }
-        let line_2_y = line2.p1.y;
-        if line_2_y <= line1_y_min || line_2_y >= line1_y_max {
-            return false;
-        }
-        true
-    } else {
-        // line1 horizontal line2 vertical
-        let line1_y = line1.p1.y;
-        let line1_x_min = line1.p1.x.min(line1.p2.x);
-        let line1_x_max = line1.p1.x.max(line1.p2.x);
-        let line2_y_min = line2.p1.y.min(line2.p2.y);
-        let line2_y_max = line2.p1.y.max(line2.p2.y);
-        if line1_y < line2_y_min || line1_y > line2_y_max {
-            return false;
-        }
-        let line_2_x = line2.p1.x;
-        if line_2_x <= line1_x_min || line_2_x >= line1_x_max {
-            return false;
-        }
-        true
-    }
+    let res = possible_rects
+        .into_iter()
+        .find(|&(p1, p2, _)| {
+            lines.iter().all(|line| {
+                let left = p1.x.max(p2.x) <= line.p1.x.min(line.p2.x);
+                let right = p1.x.min(p2.x) >= line.p1.x.max(line.p2.x);
+                let above = p1.y.max(p2.y) <= line.p1.y.min(line.p2.y);
+                let below = p1.y.min(p2.y) >= line.p1.y.max(line.p2.y);
+                left || right || above || below
+            })
+        })
+        .expect("not find")
+        .2;
+    Ok(res.to_string())
 }
 
 #[cfg(test)]
