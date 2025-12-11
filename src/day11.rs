dc940979b7fd16de3a8e490cd06249f5a6194443
nyaaar <nyaaarlathotep@163.com>
2025-12-11 17:55:18 +0800

    day11part2 unfinished

--- src/day11.rs
+++ src/day11.rs
@@ -2,7 +2,7 @@ use std::collections::{HashMap, HashSet};
 
 pub fn part01(input: &str) -> Result<String, &str> {
     let m = parse(input);
-    let res = paths("you","out",&mut HashSet::new(),&m);
+    let res = paths("you", "out", &mut HashSet::new(), &m);
     // eprintln!("paths:{:?}",res);
     Ok(res.len().to_string())
 }
@@ -40,8 +40,8 @@ fn paths<'a>(
     let mut res = vec![];
     if let Some(nexts) = m.get(now) {
         for &next in nexts {
-            let succeed_paths=paths(next,end,visited,m);
-            for mut path in succeed_paths{
+            let succeed_paths = paths(next, end, visited, m);
+            for mut path in succeed_paths {
                 path.push(now);
                 res.push(path);
             }
@@ -52,7 +52,45 @@ fn paths<'a>(
 }
 
 pub fn part02(input: &str) -> Result<String, &str> {
-    Ok("solved part 2".to_string())
+    let m = parse(input);
+    let all_paths = paths("svr", "out", &mut HashSet::new(), &m);
+    let res: Vec<_> = all_paths
+        .into_iter()
+        .filter(|path| path.contains(&"fft") && path.contains(&"dac"))
+        .collect();
+    // eprintln!("paths:{:?}", res);
+    Ok(res.len().to_string())
+}
+fn part2_paths<'a>(
+    now: &'a str,
+    end: &'a str,
+    path: Vec<&'a str>,
+    visited: &mut HashSet<&'a str>,
+    m: &'a HashMap<&str, Vec<&str>>,
+) -> Vec<Vec<&'a str>> {
+    if visited.contains(now) {
+        return vec![];
+    }
+    if now == end {
+        if path.contains(&"fft") && path.contains(&"dac") {
+            return vec![path];
+        }
+        return vec![];
+    }
+    visited.insert(now);
+    let mut res = vec![];
+    if let Some(nexts) = m.get(now) {
+        for &next in nexts {
+            let mut new_path = path.clone();
+            new_path.push(now);
+            let succeed_paths = part2_paths(next, end, new_path, visited, m);
+            for path in succeed_paths {
+                res.push(path);
+            }
+        }
+    }
+    visited.remove(now);
+    res
 }
 
 #[cfg(test)]
@@ -72,8 +110,21 @@ iii: out";
     fn test_part1() {
         assert_eq!(part01(&INPUT).unwrap(), "5");
     }
+    const PART2_INPUT: &str = "svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out";
     #[test]
     fn test_part2() {
-        assert_eq!(part02(&INPUT).unwrap(), "6");
+        assert_eq!(part02(&PART2_INPUT).unwrap(), "2");
     }
 }
